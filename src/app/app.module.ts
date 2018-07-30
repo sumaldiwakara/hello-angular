cd77beadca7121e8e83445a568e0e0adddb502a6
Sumal Diwakara <sumal.diwakara@mitel.com>
2018-07-30 15:26:17 -0400

    Toolbar app in existing hello-angular app in AWS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ClConsoleComponentsModule } from '@mitel/cl-console-components';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	ClConsoleComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
